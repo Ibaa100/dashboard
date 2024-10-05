9d885b8d731f85e5372d0d960033f08565becb87
Ibaa <ebaahmad12@gmail.com>
2024-10-05 15:45:12 +0300

    Add project files.

---
+++ dashboardPL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dashboardPL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
